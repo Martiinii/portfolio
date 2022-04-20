736fbbfa2d50db946aafc8871320b2bd294ce61c
Martini <me.martiinii@gmail.com>
2022-04-20 19:27:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <header className="text-6xl">
+          <h1>I'm <span className="font-display bg-gradient-to-r from-yellow-400 to-amber-600 bg-clip-text text-transparent">Martin</span>,<br />Full-stack web developer.</h1>
+        </header>
+      </main>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': "Montserrat",
+      'display': 'Fredoka One'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
