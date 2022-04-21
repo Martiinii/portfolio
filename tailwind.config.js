3daed04522dbb63ededd4ec9a368bb377d36a751
Martini <me.martiinii@gmail.com>
2022-04-21 21:43:56 +0200

    Moved font to 'extend'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    fontFamily: {
-      'sans': "Montserrat",
-      'display': 'Fredoka One'
+    extend: {
+      fontFamily: {
+        'sans': "Montserrat",
+        'display': 'Fredoka One'
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
