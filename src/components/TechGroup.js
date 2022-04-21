fee6ac30f39ef2012e0fd05385d321bac7a8c07e
Martini <me.martiinii@gmail.com>
2022-04-21 23:53:33 +0200

    Added skills

--- src/components/TechGroup.js
+++ src/components/TechGroup.js
@@ -1,20 +1,72 @@
-import { FirebaseSkillTag, JSSkillTag, NextSkillTag, ReactSkillTag, TailwindSkillTag } from "./SkillTag"
+import Breaker from "./Breaker"
+import { BootstrapSkillTag, CPPSkillTag, CSSSkillTag, ExpressSkillTag, FigmaSkillTag, FirebaseSkillTag, FramerMotionSkillTag, GatsbySkillTag, GitSkillTag, JavaSkillTag, JSSkillTag, MariaDBSkillTag, MongoDBSkillTag, NextSkillTag, NodeSkillTag, PHPSkillTag, PythonSkillTag, ReactSkillTag, SQLSkillTag, SvelteSkillTag, TailwindSkillTag, ThreeSkillTag } from "./SkillTag"
 
-const TechGroup = ({title}) => {
+const TechGroup = ({ title, children }) => {
     return (
         <article>
             <h3 className="text-center font-medium text-lg xs:text-xl whitespace-nowrap">{title}</h3>
             <hr className="border-neutral-500 my-3" />
 
-            <div className="flex flex-col gap-3 w-fit mx-auto text-base sm:text-lg">
-                <JSSkillTag />
-                <TailwindSkillTag />
-                <ReactSkillTag />
-                <NextSkillTag />
-                <FirebaseSkillTag />
+            <div className="flex flex-col gap-3 mx-auto text-base sm:text-lg">
+                {children}
             </div>
         </article>
     )
 }
 
-export default TechGroup
+const FrontEndTechGroup = () => {
+    return (
+        <TechGroup title="Front end">
+            <ReactSkillTag />
+            <GatsbySkillTag />
+            <SvelteSkillTag />
+
+            <Breaker />
+
+            <TailwindSkillTag />
+            <CSSSkillTag />
+            <BootstrapSkillTag />
+
+            <Breaker />
+
+            <FramerMotionSkillTag />
+            <ThreeSkillTag />
+        </TechGroup>
+    )
+}
+
+const BackEndTechGroup = () => {
+    return (
+        <TechGroup title="Back end">
+            <NextSkillTag />
+            <ExpressSkillTag />
+            <PHPSkillTag />
+            <FirebaseSkillTag />
+
+            <Breaker />
+
+            <SQLSkillTag />
+            <MariaDBSkillTag />
+            <MongoDBSkillTag />
+        </TechGroup>
+    )
+}
+
+const OtherTechGroup = () => {
+    return (
+        <TechGroup title="Others">
+            <JSSkillTag />
+            <JavaSkillTag />         
+            <PythonSkillTag />
+            <CPPSkillTag />
+
+            <Breaker />
+
+            <FigmaSkillTag />
+            <NodeSkillTag />
+            <GitSkillTag />
+        </TechGroup>
+    )
+}
+
+export { FrontEndTechGroup, BackEndTechGroup, OtherTechGroup }
