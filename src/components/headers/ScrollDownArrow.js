7adf9faca118722922cf90c9da1583fcbb086d0c
Martini <me.martiinii@gmail.com>
2022-04-22 15:22:49 +0200

    Visual change

--- src/components/headers/ScrollDownArrow.js
+++ src/components/headers/ScrollDownArrow.js
@@ -3,21 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
 
 const ScrollDownArrow = () => {
-    return (
-        <motion.div
-        className="mx-auto text-6xl"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 2, duration: 2 }}
+  return (
+    <motion.div
+      className="ml-auto text-3xl flex gap-5 items-center"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 2, duration: 2 }}
+    >
+      <span className="text-xl">Scroll to discover</span>
+      <motion.div
+        animate={{ y: [-5, 5, -5], color: ['#525252', '#262626', '#525252'] }}
+        transition={{ repeat: Infinity, delay: 2, duration: 2 }}
       >
-        <motion.div
-          animate={{ y: [-50, 0, -50], color: ['#525252', '#262626', '#525252'] }}
-          transition={{ repeat: Infinity, delay: 2, duration: 2 }}
-        >
-          <FontAwesomeIcon icon={faArrowDownLong} />
-        </motion.div>
+        <FontAwesomeIcon icon={faArrowDownLong} />
       </motion.div>
-    )
+    </motion.div>
+  )
 }
 
 export default ScrollDownArrow;
