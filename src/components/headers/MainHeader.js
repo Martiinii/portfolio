0a9407215789c4d325ce6275e716b6492f59420a
Martini <me.martiinii@gmail.com>
2022-04-26 14:53:43 +0200

    Moved style

--- src/components/headers/MainHeader.js
+++ src/components/headers/MainHeader.js
@@ -14,7 +14,7 @@ const MainHeader = () => {
     return (
         <header className="header px-5 xs:px-10 md:pl-20 lg:pl-30 xl:pl-40 text-4xl xs:text-6xl md:text-5xl lg:text-7xl">
             <ShowWhenVisible variants={visibleVariants.slideDown(50)} delay={.3}>
-                <h1 className="mb-10" >I'm <motion.span className="fancy-text" variants={variants} whileHover="animate" whileTap="animate" >Martin</motion.span>,<br />Web developer.</h1>
+                <h1 className="mb-10" >I'm <motion.span className="fancy-header" variants={variants} whileHover="animate" whileTap="animate" >Martin</motion.span>,<br />Web developer.</h1>
 
                 <ShowWhenVisible variants={visibleVariants.slideUp(50)} delay={1.1} className="flex">
 
