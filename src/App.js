39eb3a131fa637e317519e9b4aad3d8a3f9d4797
Martini <me.martiinii@gmail.com>
2022-04-26 14:53:54 +0200

    Added 'About me' section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./components/Navbar";
 import ScrollDownArrow from "./components/headers/ScrollDownArrow";
 import { ShowWhenVisible, visibleVariants } from "./components/ShowWhenVisible";
 import TechGroups from "./components/skills/TechGroups";
+import FancyText from "./components/about/FancyText";
+import WavyText from "./components/about/WavyText";
 
 function App() {
   return (
@@ -17,7 +19,22 @@ function App() {
       </section>
 
       <section className="bg-secondary min-h-screen" id="about">
-        
+        <H2>About me</H2>
+        <article className="max-w-4xl text-lg">
+          <ShowWhenVisible delay={.5} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+            <p>I am a Full-Stack Web Developer from Poland. <span className="whitespace-nowrap">I mainly</span> do the Front-End, less often the Back-End. <span className="whitespace-nowrap">I love</span> minimalist and modern websites.</p>
+          </ShowWhenVisible>
+
+          <ShowWhenVisible delay={.8} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+            <p className="leading-10"><FancyText>UI / UX</FancyText> is my top priority.</p>
+            <p className="leading-7"><WavyText>Animations</WavyText> that complement the content are on the second place.</p>
+          </ShowWhenVisible>
+
+          <ShowWhenVisible delay={1.1} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+            <p>I am eager to learn new technologies and <span className="whitespace-nowrap">I willingly</span> study new topics that interest me. <span className="whitespace-nowrap">I can</span> spend hours upon new things, thoroughly analyzing how they work under the hood. <span className="whitespace-nowrap">I pay</span> a lot of attention to optimizing and organizing the code.</p>
+            <p>My interests are astronomy, rockets, astrophotography and Rubik’s cubes.</p>
+          </ShowWhenVisible>
+        </article>
       </section>
 
       <section className="bg-primary min-h-screen" id="skills">
