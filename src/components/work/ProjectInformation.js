31c8c0e0c447d736daf5390b900520d670b9bb3b
Martini <me.martiinii@gmail.com>
2022-05-07 13:18:23 +0200

    Added animation, style moved to css file

--- src/components/work/ProjectInformation.js
+++ src/components/work/ProjectInformation.js
@@ -1,30 +1,37 @@
 import React from "react";
 import Breaker from "../Breaker";
 import ImageSlider from "../ImageSlider"
+import { ShowWhenVisible, visibleVariants } from "../ShowWhenVisible";
 import { FigmaSkillTag, FramerMotionSkillTag, JSSkillTag, NextSkillTag, TailwindSkillTag } from "../skills/SkillTag";
 
 const ProjectInformation = ({ path, images, title, skills = [], children, direction = 0 }) => {
     return (
-        <article className={`flex gap-5 flex-col w-fit mx-auto ${direction === 0 ? 'items-start lg:flex-row' : 'items-end lg:items-start lg:flex-row-reverse'}`}>
-            <header className="flex flex-col gap-5 bg-white p-5 rounded-xl shadow-lg max-w-xl flex-shrink-[2]">
-                <h3 className="text-xl font-bold">{title}</h3>
-                <div className="w-full">{children}</div>
-            </header>
+        <ShowWhenVisible
+            variants={visibleVariants.slideUp(50)}
+            className="mb-32"
+        >
+            <article className={`project-info ${direction === 0 ? 'items-start lg:flex-row' : 'items-end lg:items-start lg:flex-row-reverse'}`}>
+                <header className="card max-w-xl flex-shrink-[2]">
+                    <h3 className="text-xl font-bold after:h-px after:inline-block text-center">{title}</h3>
+                    <Breaker />
+                    <div className="w-full">{children}</div>
+                </header>
 
-            <div className="flex flex-col bg-white rounded-xl gap-5 p-5">
-                <ImageSlider
-                    path={path}
-                    images={images}
-                    className="max-w-3xl"
-                />
-                <Breaker />
-                <div className="flex flex-wrap gap-4 justify-center text-sm">
-                    {skills.map((skill, i) => {
-                        return React.cloneElement(skill, { key: i })
-                    })}
+                <div className="card">
+                    <ImageSlider
+                        path={path}
+                        images={images}
+                        className="max-w-3xl"
+                    />
+                    <Breaker />
+                    <div className="flex flex-wrap gap-4 justify-center text-sm">
+                        {skills.map((skill, i) => {
+                            return React.cloneElement(skill, { key: i })
+                        })}
+                    </div>
                 </div>
-            </div>
-        </article>
+            </article>
+        </ShowWhenVisible>
     )
 }
 
@@ -48,7 +55,8 @@ const WOMProject = () => {
                 <FigmaSkillTag />,
             ]}
         >
-            Web application for learning / revising words for exams. It doesn't require any account, data is stored in browser's local storage.
+            <p>Web application for learning / revising words or concepts for exams. It doesn't require any account, data is stored in browser's local storage.</p>
+            <p>Built in support for multiple lists.</p>
         </ProjectInformation>
     )
 }
