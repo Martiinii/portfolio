12c0a2f70d92e01e7a1401f0ee0dd32ba55434ff
Martini <me.martiinii@gmail.com>
2022-05-08 18:16:28 +0200

    Better animations

--- src/App.js
+++ src/App.js
@@ -23,16 +23,16 @@ function App() {
       <section className="bg-secondary min-h-screen" id="about">
         <H2>About me</H2>
         <article className="max-w-4xl text-lg mx-auto">
-          <ShowWhenVisible delay={.5} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+          <ShowWhenVisible delay={.5} variants={visibleVariants.slideRight(80)} type="spring" duration={1.5}>
             <p>I&nbsp;am a Full-Stack Web Developer from Poland. I&nbsp;mainly do the Front-End, less often the Back-End. I&nbsp;love minimalist and modern websites.</p>
           </ShowWhenVisible>
 
-          <ShowWhenVisible delay={.8} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+          <ShowWhenVisible delay={.8} variants={visibleVariants.slideRight(80)} type="spring" duration={1.5}>
             <p className="leading-10"><FancyText>UI / UX</FancyText> is my top priority.</p>
             <p className="leading-7"><WavyText>Animations</WavyText> that complement the content are on the second place.</p>
           </ShowWhenVisible>
 
-          <ShowWhenVisible delay={1.1} variants={visibleVariants.slideRight("100%")} type="spring" duration={1.5}>
+          <ShowWhenVisible delay={1.1} variants={visibleVariants.slideRight(80)} type="spring" duration={1.5}>
             <p>I&nbsp;am eager to learn new technologies and I&nbsp;willingly study new topics that interest me. I&nbsp;can spend hours upon new things, thoroughly analyzing how they work under the hood. I&nbsp;pay a lot of attention to optimizing and organizing the code.</p>
             <p>My interests are astronomy, rockets, astrophotography and Rubik's cubes.</p>
           </ShowWhenVisible>
