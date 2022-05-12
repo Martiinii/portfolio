d878614ef0e110742190509a0f5f0e1bf9ff1a00
Martini <me.martiinii@gmail.com>
2022-05-12 15:21:09 +0200

    Code organization, second project

--- src/components/work/ProjectInformation.js
+++ src/components/work/ProjectInformation.js
@@ -2,34 +2,53 @@ import React from "react";
 import Breaker from "../Breaker";
 import ImageSlider from "../ImageSlider"
 import { ShowWhenVisible, visibleVariants } from "../ShowWhenVisible";
-import { FigmaSkillTag, FramerMotionSkillTag, JSSkillTag, NextSkillTag, TailwindSkillTag } from "../skills/SkillTag";
+import { CSSSkillTag, FigmaSkillTag, FramerMotionSkillTag, HTMLSkillTag, JSSkillTag, NextSkillTag, PHPSkillTag, SQLSkillTag, TailwindSkillTag } from "../skills/SkillTag";
+
+const ProjectHeader = ({ description, title, direction }) => {
+    return (
+        <header className={`card max-w-sm lg:max-w-full mb-auto ${direction === 1 ? 'justify-self-end lg:order-2' : ''}`}>
+            <h3 className="text-xl font-bold after:h-px after:inline-block text-center">{title}</h3>
+            <Breaker />
+            <div className="w-full">{description}</div>
+        </header>
+    )
+}
+
+const ProjectCard = ({ path, images, skills }) => {
+    return (
+        <div className="card lg:col-span-2">
+            <ImageSlider
+                path={path}
+                images={images}
+            />
+            <Breaker />
+            <div className="flex flex-wrap gap-4 justify-center text-sm">
+                {skills.map((skill, i) => {
+                    return React.cloneElement(skill, { key: i })
+                })}
+            </div>
+        </div>
+    )
+}
 
 const ProjectInformation = ({ path, images, title, skills = [], children, direction = 0 }) => {
     return (
         <ShowWhenVisible
             variants={visibleVariants.slideUp(50)}
-            className="mb-32"
+            className="mb-32 last-of-type:mb-0"
         >
-            <article className={`project-info ${direction === 0 ? 'items-start lg:flex-row' : 'items-end lg:items-start lg:flex-row-reverse'}`}>
-                <header className="card max-w-xl flex-shrink-[2]">
-                    <h3 className="text-xl font-bold after:h-px after:inline-block text-center">{title}</h3>
-                    <Breaker />
-                    <div className="w-full">{children}</div>
-                </header>
+            <article className={`project-info ${direction === 0 ? 'before:-left-3 sm:before:-left-5' : 'before:-right-3 sm:before:-right-5'}`}>
+                <ProjectHeader
+                    description={children}
+                    title={title}
+                    direction={direction}
+                />
 
-                <div className="card">
-                    <ImageSlider
-                        path={path}
-                        images={images}
-                        className="max-w-3xl"
-                    />
-                    <Breaker />
-                    <div className="flex flex-wrap gap-4 justify-center text-sm">
-                        {skills.map((skill, i) => {
-                            return React.cloneElement(skill, { key: i })
-                        })}
-                    </div>
-                </div>
+                <ProjectCard
+                    path={path}
+                    images={images}
+                    skills={skills}
+                />
             </article>
         </ShowWhenVisible>
     )
@@ -61,4 +80,30 @@ const WOMProject = () => {
     )
 }
 
-export { WOMProject }
+const SMRNDZProject = () => {
+    return (
+        <ProjectInformation
+            title="Smrekówka and Niedźwiedzia Chata"
+            path="images/smrndz/"
+            images={[
+                "1.webp",
+                "2.webp",
+                "3.webp",
+                "4.webp",
+                "5.webp",
+            ]}
+            skills={[
+                <HTMLSkillTag />,
+                <CSSSkillTag />,
+                <JSSkillTag />,
+                <PHPSkillTag />,
+                <SQLSkillTag />,
+            ]}
+            direction={1}
+        >
+            <p>Website with information about two houses for rent. Includes an easy-to-use booking system with administration panel to manage reservations.</p>
+        </ProjectInformation>
+    )
+}
+
+export { WOMProject, SMRNDZProject }
